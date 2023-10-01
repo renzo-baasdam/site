a68be0490831b1ed02d9e81d9c9aa4a6e4172527
Renzo Baasdam <renzobaasdam@gmail.com>
2023-10-01 22:19:40 +0200

    style: tab width of 2 and as spaces

--- services/ui-svelte/src/routes/terminal/+page.svelte
+++ services/ui-svelte/src/routes/terminal/+page.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { CustomTerminal } from './terminal';
-	import 'xterm/css/xterm.css';
-	let terminal: any; //creates a var to bind the dom element
+  import { onMount } from 'svelte';
+  import { CustomTerminal } from './terminal';
+  import 'xterm/css/xterm.css';
+  let terminal: any; //creates a var to bind the dom element
 
-	onMount(async () => {
-		const term = await CustomTerminal.init();
-		term.start(terminal);
-	});
+  onMount(async () => {
+    const term = await CustomTerminal.init();
+    term.start(terminal);
+  });
 </script>
 
 <svelte:head>
-	<title>Terminal</title>
-	<meta name="description" content="About this app" />
+  <title>Terminal</title>
+  <meta name="description" content="About this app" />
 </svelte:head>
 
 <div class="text-column">
-	<link rel="stylesheet" href="xterm/css/xterm.css" />
-	<div id="terminal" bind:this={terminal} />
+  <link rel="stylesheet" href="xterm/css/xterm.css" />
+  <div id="terminal" bind:this={terminal} />
 </div>
--- services/ui-svelte/src/routes/terminal/terminal.ts
+++ services/ui-svelte/src/routes/terminal/terminal.ts
@@ -1,73 +1,73 @@
 import type { Terminal } from 'xterm';
 
 export class CustomTerminal {
-	public static async init(): Promise<CustomTerminal> {
-		const xterm = await import('xterm'); // import when the dom is ready
-		return new CustomTerminal(
-			new xterm.Terminal({
-				cursorBlink: true
-			})
-		);
-	}
+  public static async init(): Promise<CustomTerminal> {
+    const xterm = await import('xterm'); // import when the dom is ready
+    return new CustomTerminal(
+      new xterm.Terminal({
+        cursorBlink: true
+      })
+    );
+  }
 
-	public start(element: HTMLElement): CustomTerminal {
-		this.terminal.open(element);
-		this.terminal.focus();
-		return this;
-	}
+  public start(element: HTMLElement): CustomTerminal {
+    this.terminal.open(element);
+    this.terminal.focus();
+    return this;
+  }
 
-	public terminal: Terminal;
+  public terminal: Terminal;
 
-	constructor(term: Terminal) {
-		this.terminal = term;
-		this.terminal.write('Hello \x1B[1;3;31muser\x1B[0m $ ');
-		this.terminal.attachCustomKeyEventHandler(this.copyPasteEventHandler);
-		this.terminal.onData(this.handleInput);
-	}
+  constructor(term: Terminal) {
+    this.terminal = term;
+    this.terminal.write('Hello \x1B[1;3;31muser\x1B[0m $ ');
+    this.terminal.attachCustomKeyEventHandler(this.copyPasteEventHandler);
+    this.terminal.onData(this.handleInput);
+  }
 
-	public copyPasteEventHandler = (arg: KeyboardEvent) => {
-		if (arg.ctrlKey && arg.code === 'KeyC' && arg.type === 'keydown') {
-			if (this.terminal.hasSelection()) {
-				const selection = this.terminal.getSelection();
-				navigator.clipboard.writeText(selection).then(
-					() => console.log('Async: Copying to clipboard was successful!'),
-					(err) => console.error('Async: Could not copy text: ', err)
-				);
-				return false;
-			}
-		}
-		if (arg.ctrlKey && arg.code === 'KeyV' && arg.type === 'keydown') {
-			return false;
-		}
-		return true;
-	};
+  public copyPasteEventHandler = (arg: KeyboardEvent) => {
+    if (arg.ctrlKey && arg.code === 'KeyC' && arg.type === 'keydown') {
+      if (this.terminal.hasSelection()) {
+        const selection = this.terminal.getSelection();
+        navigator.clipboard.writeText(selection).then(
+          () => console.log('Async: Copying to clipboard was successful!'),
+          (err) => console.error('Async: Could not copy text: ', err)
+        );
+        return false;
+      }
+    }
+    if (arg.ctrlKey && arg.code === 'KeyV' && arg.type === 'keydown') {
+      return false;
+    }
+    return true;
+  };
 
-	handleInput = (input: string) => {
-		console.log('handle input:', input);
-		switch (input) {
-			case '\u0003': // Ctrl+C without selection
-				this.terminal.write('^C');
-				this.prompt();
-				break;
-			case '\r': // Enter
-				this.prompt();
-				break;
-			case '\u007F': // Backspace (DEL)
-				// Todo: Do not delete the prompt
-				this.terminal.write('\b \b');
-				break;
-			default:
-				if (
-					(input >= String.fromCharCode(0x20) && input <= String.fromCharCode(0x7e)) ||
-					input >= '\u00a0'
-				) {
-					//command += e;
-					this.terminal.write(input);
-				}
-		}
-	};
+  handleInput = (input: string) => {
+    console.log('handle input:', input);
+    switch (input) {
+      case '\u0003': // Ctrl+C without selection
+        this.terminal.write('^C');
+        this.prompt();
+        break;
+      case '\r': // Enter
+        this.prompt();
+        break;
+      case '\u007F': // Backspace (DEL)
+        // Todo: Do not delete the prompt
+        this.terminal.write('\b \b');
+        break;
+      default:
+        if (
+          (input >= String.fromCharCode(0x20) && input <= String.fromCharCode(0x7e)) ||
+          input >= '\u00a0'
+        ) {
+          //command += e;
+          this.terminal.write(input);
+        }
+    }
+  };
 
-	prompt = () => {
-		this.terminal.write('\r\n$ ');
-	};
+  prompt = () => {
+    this.terminal.write('\r\n$ ');
+  };
 }
